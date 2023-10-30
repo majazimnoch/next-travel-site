93c94e7d3610773c62de872705d277f5ea2516db
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-10-30 16:33:49 +0100

    kick off with the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Head from "next/head"
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen">
+    <div className="bg-black min-h-screen">
       <Head>
         <title>2Travel site</title>
         <meta name="description" content="A simple travel website" />
       </Head>
 
-      <main className="container mx"></main>
+      <main className="container mx-auto py-10 px-4 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold-mb-8 text-white">Travel site</h1>
+      </main>
     </div>
   )
 }
